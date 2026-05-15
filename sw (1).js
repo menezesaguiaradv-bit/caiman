bb76983ed7baf7c68c67af74729b0f2f7d9cfaa7
menezesaguiaradv-bit <menezesaguiar.adv@gmail.com>
2026-05-15 15:17:00 -0300

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,33 @@
+const CACHE = "caiman-agro-v1";
+const ASSETS = [
+  "/caiman/",
+  "/caiman/index.html"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
