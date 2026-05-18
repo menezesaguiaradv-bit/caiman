6cca91c54a52a619f8a787833d89288848539d3b
menezesaguiaradv-bit <menezesaguiar.adv@gmail.com>
2026-05-18 16:30:49 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+// Service Worker - Caiman Agro
+const CACHE = 'caiman-v1';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
